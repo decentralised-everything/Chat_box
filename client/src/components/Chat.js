56f0c740949552e7669d69078293eb998cc4dcb3
Rishabh Barnwal <f20201677@pilani.bits-pilani.ac.in>
2021-04-02 17:36:26 +0530

    zambie

---
+++ client/src/components/Chat.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chat = () => {
+  return <h1>CHAT</h1>;
+};
+
+export default Chat;
---
+++ server/index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const app = express();
