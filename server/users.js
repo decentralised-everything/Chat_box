80e241e0a2ceb811eb1542f22a7dd535ad2a60bf
Rishabh Barnwal <f20201677@pilani.bits-pilani.ac.in>
2021-05-13 12:33:29 +0530

    deploying frontend temperorily

--- server/users.js
+++ server/users.js
@@ -29,4 +29,3 @@ const getUser = (id) => users.find((user) => user.id === id);
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
 module.exports = { addUser, removeUser, getUser, getUsersInRoom };
-//
