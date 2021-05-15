784c0763abee8ba3f4f1f051580cfa85ac6e4260
Rishabh Barnwal <f20201677@pilani.bits-pilani.ac.in>
2021-05-15 11:49:50 +0530

    finishing touches

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -7,14 +7,15 @@ import "./InfoBar.css";
 
 const InfoBar = ({ room }) => (
   <div className="infoBar">
-    <div className="leftInnerContainer">
+    <div className="LeftInnerContainer">
       <img className="onlineIcon" src={onlineIcon} alt="online icon" />
       <h3>{room}</h3>
-    </div>
-    <div className="rightInnerContainer">
-      <a href="/">
-        <img src={closeIcon} alt="close icon" />
-      </a>
+
+      <div className="RightInnerContainer">
+        <a href="/">
+          <img src={closeIcon} alt="close icon" />
+        </a>
+      </div>
     </div>
   </div>
 );
