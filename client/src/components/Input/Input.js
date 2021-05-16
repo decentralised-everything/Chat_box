8d191a472fa51b722cb2b0d797a9246da7e1b459
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-05-16 17:31:40 +0400

    just try

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -36,22 +36,27 @@ const ToggleEmoji = ({ showEmoji, setMessage }) => {
   if (showEmoji) {
     return <ListEmoji setMessage={setMessage} className="listEmoji" />;
   }
+  else return null;
 };
 
 // z-index = 1
 const ListEmoji = ({ setMessage }) => {
-  emojiArray.map((Emoji) => {
-    return (
-      <button
-        className="emoji"
-        onClick={({ target: { value } }) =>
-          setMessage(Emojifier(value) + Emoji)
-        }
-      >
-        {Emoji}{" "}
-      </button>
-    );
-  });
+  return (
+    <>
+      {emojiArray.map((Emoji) => {
+        return (
+          <button
+            className="emoji"
+            onClick={({ target: { value } }) =>
+              setMessage(Emojifier(value) + Emoji)
+            }
+          >
+            {Emoji}{" "}
+          </button>
+        );
+      })}
+    </>
+  );
 };
 
 export default Input;
