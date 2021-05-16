8803aecf92dae727950e34afd9b92c4cb92c2666
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2021-05-16 17:13:45 +0400

    emoji

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -7,39 +7,39 @@ import emojiArray from "./emoji.array";
 import SendIcon from "@material-ui/icons/Send";
 import SentimentSatisfiedSharpIcon from "@material-ui/icons/SentimentSatisfiedSharp";
 
-const [showEmoji, setShowEmoji] = useState(false);
+const Input = ({ setMessage, sendMessage, message }) => {
+  const [showEmoji, setShowEmoji] = useState(false);
+  return (
+    <form className="form">
+      <input
+        className="input"
+        type="text"
+        placeholder="Type a message..."
+        value={message}
+        onChange={({ target: { value } }) => setMessage(Emojifier(value))}
+        onKeyPress={(event) =>
+          event.key === "Enter" ? sendMessage(event) : null
+        }
+      />
+      <ToggleEmoji showEmoji={showEmoji} />
 
-const Input = ({ setMessage, sendMessage, message }) => (
-  <form className="form">
-    <input
-      className="input"
-      type="text"
-      placeholder="Type a message..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(Emojifier(value))}
-      onKeyPress={(event) =>
-        event.key === "Enter" ? sendMessage(event) : null
-      }
-    />
-    <ToggleEmoji />
-
-    <button className="EmojiButton" onClick={() => setShowEmoji(!showEmoji)}>
-      <SentimentSatisfiedSharpIcon fontSize="medium" />
-    </button>
-    <button className="sendButton" onClick={(e) => sendMessage(e)}>
-      <SendIcon fontSize="medium" />
-    </button>
-  </form>
-);
-
-const ToggleEmoji = () => {
+      <button className="EmojiButton" onClick={() => setShowEmoji(!showEmoji)}>
+        <SentimentSatisfiedSharpIcon fontSize="medium" />
+      </button>
+      <button className="sendButton" onClick={(e) => sendMessage(e)}>
+        <SendIcon fontSize="medium" />
+      </button>
+    </form>
+  );
+};
+const ToggleEmoji = ({ showEmoji }) => {
   if (showEmoji) {
     return <ListEmoji className="listEmoji" />;
   }
 };
 
 // z-index = 1
-const ListEmoji = () => {
+const ListEmoji = ({setMessage}) => {
   emojiArray.map((Emoji) => {
     return (
       <button
