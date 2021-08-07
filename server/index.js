402a89598ab71f7de35fdaf2a1201bb8eb40135a
Rishabh Barnwal <f20201677@pilani.bits-pilani.ac.in>
2021-08-07 23:07:09 +0530

    shifting

---
+++ server/index.js
@@ -0,0 +1,79 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const cors = require("cors");
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
+const router = require("./router");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+app.use(router);
+
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit("message", {
+      user: "admin",
+      data: {
+        text: `${user.name}, welcome to room ${user.room}.`,
+        image: "",
+      },
+    });
+    socket.broadcast.to(user.room).emit("message", {
+      user: "admin",
+      data: { text: `${user.name} has joined!`, image: null },
+    });
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    console.log(message);
+    const user = getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", { user: user.name, data: message });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        data: {
+          text: `${user.name} has left.`,
+          image: "",
+        },
+      });
+    }
+  });
+});
+
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started. `)
+);
