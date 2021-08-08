f2932c0d238ded506e2f114a018032aa38a5e042
Rishabh Barnwal <f20201677@pilani.bits-pilani.ac.in>
2021-08-09 01:40:42 +0530

    added tailwind in react branch, was getting bored

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Chat from "./components/Chat/Chat";
 import Join from "./components/Join/Join";
+import "./styles/output.css";
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
